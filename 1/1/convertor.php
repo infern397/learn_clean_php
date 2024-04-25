362eb6eacef45bada38c14dd31da196d72eb26e5
infern <semenmochalov55@gmail.com>
2024-04-25 08:52:07 +0700

    1/1 exercise

---
+++ 1/1/convertor.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Конвертор</title>
+    </head>
+    <body>
+        <h1>Преобразование из дюймов в сантиметры</h1>
+        <?php
+            $ins = 20;
+            $cents = $ins * 2.54;
+            $cents = round($cents);
+        ?>
+        <p><?php echo $ins ?> дюймов = <?php echo $cents?> сантиметров.</p>
+        <?php
+            $ins = 27;
+            $cents = round($ins * 2.54);
+        ?>
+        <p><?php echo $ins ?> дюймов = <?php echo $cents?> сантиметров.</p>
+    </body>
+</html>
