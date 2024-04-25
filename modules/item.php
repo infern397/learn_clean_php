285ae2f39188d604297366834f213f994428e701
infern <semenmochalov55@gmail.com>
2024-04-25 23:44:34 +0700

    5/2 exercise

--- modules/item.php
+++ modules/item.php
@@ -1,20 +1,19 @@
 <?php
-    require_once 'modules/data.php';
-    $item = $arr_images[$_GET['index']];
+    $item = $arr_images[$index];
 ?>
 <!doctype html>
 <html>
     <head>
         <meta charset="UTF-8">
         <link href="/styles.css" rel="stylesheet" type="text/css">
-        <title><?php echo $item['desc'] ?> :: Фотогаллерея</title>
+        <title>Фотогалерея</title>
     </head>
     <body>
         <h1>Фотогалерея</h1>
         <h2><?php echo $item['desc'] ?></h2>
         <p><a href="/">На главную</a></p>
         <section id="gallery-item">
-            <img src="images/<?php echo $item['src']?>" alt="">
+            <img src="/images/<?php echo $item['src'] ?>">
         </section>
         <p><a href="/">На главную</a></p>
     </body>
---
+++ modules/router.php
@@ -0,0 +1,9 @@
+<?php
+$request_path = $_GET['route'];
+
+if ($request_path == '')
+    require $base_path . 'modules\list.php';
+else {
+    $index = (integer)$request_path;
+    require $base_path . 'modules\item.php';
+}
