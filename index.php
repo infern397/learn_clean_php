ef04594cfc2748ef5986a6d08cd40b5288cc8960
infern <semenmochalov55@gmail.com>
2024-04-26 10:23:43 +0700

    5/7 exercise

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-$base_path = __DIR__ . '\\';
-require_once $base_path . 'modules\data.php';
-require_once $base_path . 'modules\router.php';
--- modules/router.php
+++
@@ -1,9 +0,0 @@
-<?php
-$request_path = $_GET['route'];
-
-if ($request_path == '')
-    require $base_path . 'modules\list.php';
-else {
-    $index = (integer)$request_path;
-    require $base_path . 'modules\item.php';
-}
